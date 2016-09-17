be96392ef6f6cfc52e75468e4316ef9e5b346cab
corenel <xxdsox@gmail.com>
2016-09-17 12:54:24 +0800

    using random.sample to generate random vocabulary list.

--- Word_dictation/dictate.py
+++ Word_dictation/dictate.py
@@ -1,5 +1,5 @@
 from os import system
-from random import randint
+from random import sample
 from time import sleep
 
 def say(word):
@@ -14,9 +14,8 @@ def read_dict(file):
 	return dic
 
 def dictate(dictionary):
-	for i in range(10):
-		index = randint(0, len(dictionary) - 1)
-		word = dictionary[index];
+	random_list = sample(dictionary, 10)
+	for word in random_list:
 		english_only = ''.join(x for x in word if ord(x) < 256)
 		say(english_only)
 		sleep(5)
